8528d74bafb44fd94242a651165229f9aa8e9a6c
Muhammad Hassan <hassanmamji9@gmail.com>
2024-09-13 21:10:29 +0500

    Add files via upload

---
+++ class 4/app.js
@@ -0,0 +1,106 @@
+// && and Operator
+
+// Logiacal Operator
+
+// Or Operator 
+
+
+// var userweight = +prompt("Enter your weight");
+
+// var time = +prompt("Enter time");
+
+// var gender = prompt("Enter your gender");
+
+// if(userweight <= 100 || (time < 6 && gender)){
+//     console.log("come to our tryout!!");
+    
+// }
+// else{
+//     console.log("better luck next time");
+    
+// }
+
+
+// let m1,m2,m3,sum,percentage
+// m1=80
+// m2=90
+// m3=98
+// sum=m1+m2+m3
+// console.log("total marks" ,sum)
+// percentage=sum/3
+// console.log('average=',percentage);
+
+
+
+
+// var marks01 = +prompt("Enter Subject 01 marks : ")
+
+// var marks02 = +prompt("Enter Subject 02 marks : ")
+
+// var marks03 = +prompt("Enter Subject 03 marks : ")
+
+// document.write("Subject 01 marks : ${marks} <br>")
+// document.write("Subject 02 marks : ${marks} <br>")
+// document.write("Subject 03 marks : ${marks} <br>")
+
+// var obtainmarks = marks01 + marks02 + marks03;
+
+// var totalmarks = 300;
+
+// document.write("Total marks : ${totalmarks} <br>")
+
+// var percentage = (obtainmarks / totalmarks) * 100 
+
+
+
+// var age = +prompt("Enter your age");
+
+// var res = prompt("")
+
+
+
+// var progLang = prompt("Enter your programming laguage");
+
+// if (progLang === "JavaScript"){
+//     var city = prompt("Enter your city");
+
+//     if (city === "Karachi"){
+
+//         var experiance = +prompt("Enter your experiance");
+
+//         if (experiance >= 2){
+//             console.log("You are Hired!!");
+//         }
+
+//         else{
+//             console.log("experiance Lao!!");            
+//         }
+        
+//     }
+    
+//     else{
+//         console.log("Karachi Ajao"); 
+//     }
+// } 
+
+// else{
+//     console.log("Js Seekho"); 
+// }
+
+
+// Array Defination :
+
+//  multiple values store in a single variable is called array
+
+
+// var num1 = 10;
+// var num2 = Hassan;
+// var num3 = 30;
+// var num4 = $;
+// var num5 = Undefined;
+
+// var num = [10,"Hassan",30,undefined];
+
+
+// console.log(num);
+
