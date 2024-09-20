d5e7aee63bdc51605d178a87684f0ac81a6babc5
Muhammad Hassan <hassanmamji9@gmail.com>
2024-09-20 21:14:49 +0500

    Add files via upload

---
+++ class 5/app.js
@@ -0,0 +1,119 @@
+// ARRAY      CHAPTER : 16 & 17        
+
+
+// var arr = [200,"hassan",undefined,"empty"]
+
+// console.log(arr);
+
+// var arr = [200,10,"hassan"]
+
+// console.log(arr[2]);
+
+
+// PUSH METHod store data in the end of an array.
+
+// var fruitsArr = ["apple","orange","banana"];
+
+// fruitsArr.push("orange","grapes");
+
+// console.log(fruitsArr);
+
+
+// Unshift METHod store data in the start of an array.
+
+// var fruitsArr = ["apple","orange","banana"];
+
+// fruitsArr.unshift("orange","apple")
+
+// console.log(fruitsArr);
+
+ 
+
+// pop last vala ko delete kre ga
+
+// var fruitsArr = ["apple","orange","banana"];
+
+// fruitsArr.pop();
+
+// console.log(fruitsArr);
+
+// shift last vala ko delete kre ga
+
+// var fruitsArr = ["apple","orange","banana"];
+
+// fruitsArr.shift();
+
+// console.log(fruitsArr);
+
+
+
+// var fruitsArr = ["apple","orange","banana"];
+
+// fruitsArr.splice(1,1);
+
+// console.log(fruitsArr);
+
+
+// fruitsArr.splice()  // to copy elements in array 
+
+
+// var fruitsArr = ["apple","pineapple","mango","orange","grapes","banana"];
+
+// fruitsArr.splice();
+
+// fruitsArr.splice(4,1,"hassan");
+
+// fruitsArr.splice(0,5,"hassan","hassan");
+
+// fruitsArr.splice(1,2,"hassan");
+
+// fruitsArr.splice(0,6);
+
+// fruitsArr.splice(-1,2);
+
+// console.log(fruitsArr);
+
+
+
+
+
+// FOR LOOP      CHAPTER : 18
+
+// loop multipe chatagri
+
+// 1) For loop
+// 2) while loop
+// 3) do-while loop
+
+
+// interection of a prcess or repetation of something is called loop
+
+// for(initialization;condition;increment/decrement){
+
+// }
+
+// for(var i = 1;i<=10;i++){
+//         console.log("hello");
+        
+// }
+
+// for(var i = 1;i<=15;i++){
+//     console.log("Hello I Am Hassan");
+    
+// }
+
+// for(var i = 1;i<=10;i++){
+//     document.write("hello world" + "<br>");
+    
+// }
+
+
+// Tables
+
+// var input = +prompt("Enter any number");
+
+// var range = +prompt("Enter range")
+
+// for (var i = 1; i <= range; i++){
+//     document.write(`${input} x ${i} = ${input *i} <br>`);
+// }
