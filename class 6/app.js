20b373fe8f53b922b90e8b32775a16e0698c811c
Muhammad Hassan <hassanmamji9@gmail.com>
2024-09-24 21:16:46 +0500

    Add files via upload

---
+++ class 6/app.js
@@ -0,0 +1,57 @@
+// CHAPTER NO : 22 (String extracting methods)
+
+// var str = prompt("Enter some text");
+// var numChars = str.length;
+
+// for (var i = 1; i < numChars; i++) {
+//     if (str.slice(i, i + 2) === "  "){
+//         alert("number found!");
+//     }
+// }
+
+// var text = `Paragraph writing is a key skill for organising and expressing ideas clearly. 
+// A paragraph typically starts with a topic sentence that introduces the 
+// main idea, followed by supporting sentences that provide details or examples. `
+
+// var indexNum = text.indexOf("introduces")
+
+// for(var i = 0; i < text.length; i++){
+//     if (text.slice(i, i + 5) === "topic"){
+//         text.slice(0, i) + "lecture" + text.slice(i + 5);
+//     }
+// }
+// console.log(text);
+
+
+// replacing method :-
+
+// var text = `The New Yorker Magazine doesn't allow the phrase World War II. They say it should be`;
+
+// console.log(text.replace("World War II","The Second World War"))
+
+
+
+// var indexNum = text.indexOf("World War II");
+
+// var startingText = text.slice(0, 50);
+
+// var replcingText = "The Second World War";
+
+// var remainText = text.slice(indexNum + 12);
+
+// console.log(startingText + replcingText + remainText);
+
+
+// JavaScript >> math >> keyword >> Object
+
+// parseInt ka function decimal accept nhi krta
+
+// Decimal Method :-
+
+// var number = +prompt("Enter any decimal number");
+
+// console.log("round",Math.round(number));
+// console.log("ceil",Math.ceil(number));
+// console.log("floor",Math.floor(number));
+
+
