cda5d7aff15fa319c1ea87aeec49dbc913dc99cd
Muhammad Hassan <hassanmamji9@gmail.com>
2024-10-01 20:39:27 +0500

    Add files via upload

---
+++ class 8/app.js
@@ -0,0 +1,59 @@
+//  PALINDROME PROGRAM
+
+
+// var input = prompt("Enter any word");
+// var reverseWord = "";
+
+// for (var i = input.length - 1; i >= 0; i-- ){
+//     reverseWord = reverseWord + input[i];
+// }
+
+// if(input === reverseWord){
+//     console.log("it's a palindrome word");
+// } else {
+//     console.log("it's not a palindrome word");    
+// }
+
+// console.log("input value: ",input);
+
+// console.log("reverseWord Value" , reverseWord);
+
+
+// DATES  METHOD
+
+
+// var date = new Date();
+
+// console.log(date);
+
+// var daysArr = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+// console.log(daysArr[date.getDay()]);
+
+// console.log(date.toLocaleDateString());
+
+
+// var currentYear = new Date();
+
+// var currentYear = getFullYear();
+
+// var user = prompt("Enter your age");
+
+// console.log();
+
+
+// DAYS METHOD
+
+
+// var FirstRamadan = new Date("March 1, 2025").getTime();
+
+// var currentDate = new Date().getTime();
+
+// var diff = FirstRamadan - currentDate;
+
+// var month = diff / (1000 * 60 * 60 * 24 );
+
+// console.log(Math.round(month));
+
+
+console.log(2<3>4);
