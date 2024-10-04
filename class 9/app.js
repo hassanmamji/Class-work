f54582185b4fdd3217f10cbe355249c6f5303a26
Muhammad Hassan <hassanmamji9@gmail.com>
2024-10-04 21:08:09 +0500

    Add files via upload

---
+++ class 9/app.js
@@ -0,0 +1,91 @@
+// function
+
+// function greet(){
+//    alert("function calling");
+//    console.log("function calling");
+
+// }
+
+// greet();         // function calling
+// greet();  
+// greet();  
+
+
+// function welcome(greet){            // parameter 
+//     console.log(greet);   
+// }
+
+// welcome("Hello");    // argument
+// welcome("Hello");    
+// welcome("Hello");    
+// welcome("Hello");    
+// welcome("Hello");    
+
+
+// function add(){
+//     var a = 20
+//     var b = 10
+//     console.log(a+b);
+
+
+// }
+
+// add();
+
+
+// var num1 = +prompt("Enter Your First Number")
+
+// var num2 = +prompt("Enter Your Second Number")
+
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+
+// }
+
+// add(num1, num2);
+
+
+// Multiply , Divide , Addition , Subtraction   (METHOD)
+
+// var num1 = +prompt("Enter Your First Number")
+
+// var num2 = +prompt("Enter Your Second Number")
+
+// var num3 = +prompt("Enter Your Third Number")
+
+
+// function add(num1, num2, num3) {
+//     console.log(" Sum ", num1 + num2 + num3);
+//     console.log(" Sub ", num1 - num2 - num3);
+//     console.log(" Multi " + num1 * num2 * num3);
+//     console.log(" Divide " + num1 / num2 / num3 );
+// }
+
+// add(num1, num2, num3);
+
+
+// function message(name , Age , city){
+//     console.log(`Hello My Name is ${name}. My Age is ${Age} years old . I Am lived in ${city}`);
+// }
+
+// message("Hassan" , 17 , "Karachi");
+
+
+// function greet(message){
+//     return message; 
+// }
+// var result = greet("function call");
+// console.log(result);
+
+
+
+// function multi(a,b){
+//     var res = a* b;
+
+//     return message;
+// }
+// var get = multi(5,6)
+// console.log(get + 10);
+
+
