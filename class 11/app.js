0b326b4f5d93da430305491125cc838682a32219
Muhammad Hassan <hassanmamji9@gmail.com>
2024-10-15 21:24:17 +0500

    Add files via upload

---
+++ class 11/app.js
@@ -0,0 +1,52 @@
+// function getDropdown(){
+//     var getDropdown = document.getElementById()
+
+//     console.log(dropdown.value);
+    
+// }
+
+
+// var h1heading = document.getElementById("heading")
+
+// h1heading.innerHTML = "<p>Hello World</p>"
+
+// console.log(h1);
+
+
+// function completePara(){
+    
+// }
+
+
+// var paragraph = document.getElementById("para");
+
+// console.log(h1heading.innerText);
+
+// console.log(paragraph.innerText);
+
+// var h1 = document.getElementById("heading");
+
+// h1.innerHTML = 
+
+
+
+// function completePara(){
+//     var para = document.getElementById("para");
+
+//     var anchor = document.getElementById("link");
+
+//     if(
+//         anchor.innerHTML === "Read More"){
+//             para.innerHTML =  ;
+
+//             anchor.innerHTML = "Read Less"
+//         }else{
+//             para.innerHTML = 
+
+//             anchir.innerHTML = "Read More"
+//         }
+
+// }
+
+
+
