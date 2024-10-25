0fbf9e6f10a1ab85be34fc8723fe129850e4de9d
Muhammad Hassan <hassanmamji9@gmail.com>
2024-10-25 21:00:45 +0500

    Add files via upload

---
+++ Class 12/app.js
@@ -0,0 +1,55 @@
+// var divElement = document.getElementById("main");
+
+// console.log(divElement.hasAttribute("class"));
+
+// console.log(divElement.getAttribute("class"));
+
+// var anchor = document.getElementById("link");
+
+// anchor.setAttribute("class" , "link");
+
+// var btn = document.getElementById("btn");
+
+// btn.setAttribute("class" , "btn");
+
+// btn.setAttribute("onclick" , "greet()");
+
+// console.log(btn.attributes[2].value);
+
+// function greet(){
+//     alert("alert calls...")
+// }
+
+// var div =
+
+// document.getElementById("main");
+
+// var h1Element = 
+
+// document.createElement("h1");
+
+// var h1Text = 
+
+// document.createTextNode("hello world");
+
+// h1Element.appendChild(h1Text);
+
+// div.Element.appendChild(h1Element);
+
+// console.log(h1Element);
+
+// btnElement.appendChild(btnText);
+
+// btnElement.setAttribute("onclick" , "")
+
+
+
+// CRUD Operation
+// C = create 
+// R = read
+// U = update
+// D = delete
+
+
+// TODO APPLICATON
+
