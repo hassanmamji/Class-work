7377fb7026d3451166bbca4d133447ecb94011a4
Muhammad Hassan <hassanmamji9@gmail.com>
2024-10-29 21:21:28 +0500

    Add files via upload

---
+++ Class 13/app.js
@@ -0,0 +1,109 @@
+// var studentObj = {
+//     stdName: "Hassan",
+//     stdAge: 17,
+//     stdRollNo: 1667,
+//     stdGmail: "hassan@gamil.com",
+//     stdCity: "karachi",
+// }
+
+// studentObj.courses = ["html", "css", "JS", "python"];
+
+// console.log(studentObj);
+
+
+// console.log(studentObj.courses);
+
+// studentObj.qualification = {
+//     Education: "bachelors",
+//     CGPA: "3.2",
+//     Address: {
+//         city: "Karachi",
+//         country: ["pakistan", "turkey", "india"]
+//     },
+// }
+
+// console.log(studentObj.qualification.Address.country[0]);
+
+// console.log(studentObj.qualification.CGPA);
+
+// console.log(studentObj.qualification.Address);
+
+// var studentObj = {
+//         stdName: "Hassan",
+//         stdAge: 17,
+//         stdRollNo: 1667,
+//         stdGmail: "hassan@gamil.com",
+//         stdCity: "karachi",
+// }    
+
+// delete studentObj.stdCity;
+// console.log(studentObj);
+
+
+
+
+// There Are 3 types of function in js
+
+// 1    Function Decalaration
+
+// function greet(){
+//     console.log("call");
+
+
+// }
+
+// 2    Anonyms Function / Regular
+
+// var func = function () {
+//     console.log("call");    
+// };
+// func();
+
+// 3    Arrow Function / Fat Arrow Function
+
+
+
+// var studentObj = {
+//     stdName: "Hassan",
+//     StdGetName: function (){
+// console.log(this.stdName);
+// console.log(studentObj.stdName);
+
+//     },
+//     stdAge: 17,
+//     stdRollNo: 1667,
+//     stdGmail: "hassan@gamil.com",
+//     stdCity: "karachi",
+// }
+
+// studentObj.StdGetName();
+
+// console.log(studentObj.StdGetName);
+
+// function student(name, age, rollno, email, city) {
+//     this.stdName = name;
+//     this.stdAge = age;
+//     this.stdRollNo = rollno;
+//     this.stdGmail = email;
+//     this.city = city;
+// }
+
+// var student1 = new student("Ahmed", 25, 1234, "Ahmed@gmail.com", "lahore");
+// var student2 = new student("Faraz", 22, 1254, "Faraz@gmail.com", "Islamabad");
+// var student3 = new student("Hassan", 17, 1211, "Hassan@gmail.com", "Karachi");
+// var student4 = new student("Rayyan", 27, 1791, "Rayyan@gmail.com", "K.P.K");
+
+
+// console.log(student1);
+
+// console.log(student2);
+
+// console.log(student3);
+
+// console.log(student4);
+
+// var arr = [1, 12, 123, 1234];
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
