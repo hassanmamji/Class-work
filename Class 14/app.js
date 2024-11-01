f343b773d0ac070275490ae009bf397c118e35e3
Muhammad Hassan <hassanmamji9@gmail.com>
2024-11-01 21:07:50 +0500

    Add files via upload

---
+++ Class 14/app.js
@@ -0,0 +1,67 @@
+// var studentObj = {
+//     stdName: "Hassan",
+//     stdAge: 17,
+//     education: {
+//         program: "computer science" ,
+//         batch: 2021 ,
+//         rollno: 13245 ,
+//         CGPA: 3.2 ,
+//     },
+//     stdRollNo: 1667 ,
+//     stdGmail: "hassan@gamil.com",
+//     stdCity: "karachi",
+// }
+
+// console.log(studentObj);
+
+
+// arr = [1, 12, 123, 1234];
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+// var students = [
+//     {
+//         id: 1,
+//         name: "hassan",
+//         city: "Karachi",
+//     },
+//     {
+//         id: 2,
+//         name: "muhammad",
+//         city: "Peshawer",
+//     },
+//     {
+//         id: 3,
+//         name: "abdullah",
+//         city: "Islamabad",
+//     },
+//     {
+//         id: 4,
+//         name: "farooq",
+//         city: "lahore",
+//     },
+// ];
+
+// for (var i = 0; i < students.length; i++){
+//     for (var key in students[i]){
+//         console.log(key, students[i][key]);    
+//     }
+// } 
+
+// gmail account create
+// firebaseAuth
+// web-app pe register
+// option web ka oper click
+// web nick name
+// uncheck
+// Register app click
+// Done
+// configuration keys
+// obj copy
+// app.js >> file >> top pe code
+// 2 radio buttons >> 2 option >> click
+// from >> link copy
+// HTML >> link pass >> script src >> me
+// authentication >> Emailpassword >> 
